8fffbe7f65bcda1e7ae56d81058f1f10f143615e
Lucas Vieira <lucas.engen.cc@gmail.com>
2022-04-25 02:14:10 -0300

    fix: corrige a configuração do cors
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,38 @@
-import express from 'express';
-import socketio from 'socket.io';
-import http from 'http';
+import express from "express";
+import socketio from "socket.io";
+import http from "http";
+import cors from "cors";
 
 const app = express();
 const httpServer = http.createServer(app);
 const mysocket = new socketio.Server(httpServer);
 
-app.use(express.static('public'));
-app.set('views', 'public');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(cors());
+app.use(express.static("public"));
+app.set("views", "public");
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
 const messages: any[] = [];
 var qtd_users = 0;
 
-mysocket.on('connection', (socket) => {
-  console.log('New Connection: ' + socket.id);
-  console.log('Total Connections: ' + mysocket.engine.clientsCount);
-  socket.emit('previousMessages', messages);
+mysocket.on("connection", (socket) => {
+  console.log("New Connection: " + socket.id);
+  console.log("Total Connections: " + mysocket.engine.clientsCount);
+  socket.emit("previousMessages", messages);
   var ip = socket.handshake.address;
   console.log(ip);
-  socket.on('sendMessage', (data) => {
-    console.log('data: ' + data);
-    data['id'] = socket.id;
-    data['ip'] = ip;
+  socket.on("sendMessage", (data) => {
+    console.log("data: " + data);
+    data["id"] = socket.id;
+    data["ip"] = ip;
     messages.push(data);
-    socket.broadcast.emit('receivedMessage', data);
+    socket.broadcast.emit("receivedMessage", data);
   });
 
   socket.on("disconnect", (reason) => {
     console.log("Disconnected: " + reason);
   });
 });
-
 
 httpServer.listen(3333);
