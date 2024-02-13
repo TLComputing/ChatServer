7f31d596c369dbfb53ad19d9b034f0cd67848a8c
Arthur <arthurarp97@gmail.com>
2024-02-12 23:55:33 -0300

    fix cors problem
    Signed-off-by: Arthur <arthurarp97@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,25 @@ import cors from "cors";
 
 const app = express();
 const httpServer = http.createServer(app);
-const mysocket = new socketio.Server(httpServer);
-
 app.use(cors());
 app.use(express.static("public"));
 app.set("views", "public");
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
+
+
+// const mysocket = new socketio.Server(httpServer);
+
+import { Server } from "socket.io";
+
+const mysocket = new Server(httpServer, {
+  cors: {
+    origin: "*", // Aqui você pode especificar origens específicas ou manter '*' para todas
+    methods: ["GET", "POST"], // Métodos HTTP permitidos durante o handshake do CORS
+    allowedHeaders: ["my-custom-header"], // Cabeçalhos permitidos
+    credentials: true // Se você precisa de credenciais como cookies ou cabeçalhos de autenticação
+  }
+});
 
 const messages: any[] = [];
 var qtd_users = 0;
