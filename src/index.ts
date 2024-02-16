eb158cfdc8c7086cdb092d4f33d86a8a1a45fe00
Lucas Vieira <lucas.engen.cc@gmail.com>
2024-02-15 22:35:59 -0300

    chore: :zap: ajusta o package.json e adiciona alguns comentários
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import express from "express";
-import socketio from "socket.io";
-import http from "http";
 import cors from "cors";
+import express from "express";
+import http from "http";
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -11,40 +10,45 @@ app.set("views", "public");
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
-
-// const mysocket = new socketio.Server(httpServer);
-
 import { Server } from "socket.io";
 
 const mysocket = new Server(httpServer, {
-  cors: {
-    origin: "*", // Aqui você pode especificar origens específicas ou manter '*' para todas
-    methods: ["GET", "POST"], // Métodos HTTP permitidos durante o handshake do CORS
-    allowedHeaders: ["my-custom-header"], // Cabeçalhos permitidos
-    credentials: true // Se você precisa de credenciais como cookies ou cabeçalhos de autenticação
-  }
+	cors: {
+		origin: "*", // Aqui você pode especificar origens específicas ou manter '*' para todas
+		methods: ["GET", "POST"], // Métodos HTTP permitidos durante o handshake do CORS
+		allowedHeaders: ["my-custom-header"], // Cabeçalhos permitidos
+		credentials: true, // Se você precisa de credenciais como cookies ou cabeçalhos de autenticação
+	},
 });
 
 const messages: any[] = [];
 var qtd_users = 0;
 
 mysocket.on("connection", (socket) => {
-  console.log("New Connection: " + socket.id);
-  console.log("Total Connections: " + mysocket.engine.clientsCount);
-  socket.emit("previousMessages", messages);
-  var ip = socket.handshake.address;
-  console.log(ip);
-  socket.on("sendMessage", (data) => {
-    console.log("data00098765555444444: " + JSON.stringify(data, null, 2));
-    data["id"] = socket.id;
-    data["ip"] = ip;
-    messages.push(data);
-    socket.broadcast.emit("receivedMessage", data);
-  });
+	console.log("New Connection: " + socket.id);
+	console.log("Total Connections: " + mysocket.engine.clientsCount);
 
-  socket.on("disconnect", (reason) => {
-    console.log("Disconnected: " + reason);
-  });
+	socket.emit("previousMessages", messages);
+	var ip = socket.handshake.address;
+	console.log(ip);
+
+	// O evento sendMessage é emitido quando um cliente envia
+	// a mensagem
+	socket.on("sendMessage", (data) => {
+		console.log("data00098765555444444: " + JSON.stringify(data, null, 2));
+		data["id"] = socket.id;
+		data["ip"] = ip;
+
+		messages.push(data);
+
+		// emite o evento "receivedMessage" e dispara a mensagem recebida para todos os demais clientes
+		// conectados
+		socket.broadcast.emit("receivedMessage", data);
+	});
+
+	socket.on("disconnect", (reason) => {
+		console.log("Disconnected: " + reason);
+	});
 });
 
-httpServer.listen(3333);
+httpServer.listen(8000);
