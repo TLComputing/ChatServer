74497488a810fc0af960252e66f8ed1771ef2ebb
Arthur <arthurarp97@gmail.com>
2024-02-17 19:35:19 -0300

    feat: add feature to send messages
    Signed-off-by: Arthur <arthurarp97@gmail.com>

--- src/websocket/handlers/connectionsHandlerService.ts
+++ src/websocket/handlers/connectionsHandlerService.ts
@@ -2,8 +2,24 @@ import { Socket } from "socket.io";
 
 export default class ConnectionsHandlerService {
 
+    private messages: any[] = [];
+
     handleUserConnected(socket: Socket) {
         console.log(`Usuário conectado: ${socket.id}`);
+        socket.on("sendMessage", (data) => {
+            var ip = socket.handshake.address;
+            console.log(data);
+            data["id"] = socket.id;
+            data["ip"] = ip;
+
+            this.messages.push(data);
+
+            // emite o evento "receivedMessage" e dispara a mensagem recebida para todos os demais clientes
+            // conectados
+            socket.broadcast.emit("receivedMessage", data);
+        });
+        
+
         socket.on("disconnect", () =>  {
             this.handleUserDisconnected(socket);
         });
